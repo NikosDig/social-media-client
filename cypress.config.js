2410d95cd13f4680024a4d1125eeca87cb7e6168
NikosDigalakis <nikos_23_chania@hotmail.com>
2023-11-09 19:21:59 +0100

    eddited cypres config and re added ngrok

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,20 @@
-import { defineConfig } from "cypress";
+// import { defineConfig } from "cypress";
 
-export default defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-});
+// export default defineConfig({
+//   e2e: {
+//     setupNodeEvents(on, config) {
+//       // implement node event listeners here
+//     },
+//   },
+// });
+
+// cypress.config.js
+export default (on, config) => {
+  if (config.env.ngrokUrl) {
+    config.baseUrl = config.env.ngrokUrl;
+  }
+
+  // other Cypress configuration options
+
+  return config;
+};
